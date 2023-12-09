0fbdb5600bc98aae0ac093073b13bbe4aeb5c25f
alexeizvonarev <117440062+alexeizvonarev@users.noreply.github.com>
2023-12-09 15:39:37 +0700

    modified:   botINIT.bat new file:   installDependencies.bat

--- botINIT.bat
+++ botINIT.bat
@@ -1,9 +1,12 @@
 @echo off
+color 2
 set /p "id=Enter bot TOKEN: "
 
 echo|set /p="{" > botconfig.json
 echo|set /p=""TOKEN": " >> botconfig.json
 echo|set /p=""%id%"" >> botconfig.json
 echo|set /p="}" >> botconfig.json
+
+echo Bot has been successfully initialised!
 
 pause
